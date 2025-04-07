692934276329a8430ec74626172e34f75c87b9b2
rafliafriza12 <rafliafriza90@gmail.com>
2025-04-07 17:03:09 +0700

    feat: add sponsor section

--- components/navbar/Index.tsx
+++ components/navbar/Index.tsx
@@ -2,7 +2,7 @@ import Logo from "../svg/Logo";
 const Navbar: React.FC = () => {
   return (
     <div className=" w-full flex justify-center items-center fixed z-10 top-0 backdrop-blur-md bg-transparent pt-10">
-      <div className=" absolute left-[5%]">
+      <div className=" absolute left-[5%] scale-[0.8]">
         <Logo />
       </div>
       <div className=" w-[1000px] h-[70px] bg-[#F3FFD0]/20 border-[#EDFACC] border-[1px] rounded-full flex items-center px-3 justify-evenly font-cinzel text-[#255245] font-bold text-[16px]">
--- components/sections/sponsor/Index.tsx
+++ components/sections/sponsor/Index.tsx
@@ -1,7 +1,82 @@
 import Marquee from "react-fast-marquee";
+import Logo from "@/components/svg/Logo";
+import InforClub from "@/components/svg/InfoClub";
 const Sponsor: React.FC = () => {
   return (
-    <div className=" w-screen h-[101px] bg-[#255245] border-y-[1px] border-[#FEFEFE] -mt-7"></div>
+    <div className=" w-screen h-[110px] py-2 bg-[#255245] border-y-[1px] border-[#FEFEFE] -mt-7">
+      <Marquee direction="left" className=" flex">
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <Logo />
+          </div>
+        </div>
+        <div className=" flex justify-center items-center h-full px-5 border-r-[1px] border-[#FEFEFE]">
+          <div className=" scale-[0.8]">
+            <InforClub />
+          </div>
+        </div>
+      </Marquee>
+    </div>
   );
 };
 
