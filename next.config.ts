a95f3f7d6ca777394f2b60b27c3b1460ae6c200e
rafliafriza12 <rafliafriza90@gmail.com>
2025-04-08 09:11:06 +0700

    fix: try to fixed ISR Error production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  compress: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60,
+  },
+  // Nonaktifkan optimasi CSS yang menggunakan critters
+  experimental: {
+    optimizeCss: false, // Ubah true menjadi false
+  },
 };
 
 export default nextConfig;
